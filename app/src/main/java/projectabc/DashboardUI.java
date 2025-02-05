5183fb64c4c1931049e16a516fdffc6900aa558b
ballMguy <supaluk29188@gmail.com>
2025-02-05 11:28:27 +0700

    ล็อกอินได้แล้ว

--- app/src/main/java/projectabc/DashboardUI.java
+++ app/src/main/java/projectabc/DashboardUI.java
@@ -7,8 +7,8 @@ import javafx.stage.Stage;
 public class DashboardUI {
     public DashboardUI(Stage primaryStage, User user) {
         VBox root = new VBox(10);
-        Label userLabel = new Label("Hello" + user.getUsername());
-        TextField amountField = new TextField();
+        Label userLabel = new Label("Hello " + user.getUsername() + "!");
+
         Button depositButton = new Button("Deposit");
         Button withdrawButton = new Button("Withdraw");
 
@@ -30,7 +30,7 @@ public class DashboardUI {
             // else balanceLabel.setText("Insufficient funds.");
         });
 
-        // root.getChildren().addAll(balanceLabel, new Label("Amount:"), amountField, depositButton, withdrawButton);
+        root.getChildren().addAll(userLabel,depositButton, withdrawButton);
         primaryStage.setScene(new Scene(root, 300, 200));
         primaryStage.show();
     }
